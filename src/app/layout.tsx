2307cd69444f9ff6753005ef0dd83b8837c6a652
ananya150 <akblockchain15@gmail.com>
2023-06-23 14:05:35 +0530

    feat: initialized nextjs project and added next themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className='flex min-h-screen flex-col'>
+            {/* <header className="container z-40 bg-background">
+              <div className="flex h-20 items-center justify-between py-6">
+                <MainNav items={marketingConfig.mainNav} />
+                <nav>
+                  <Link
+                    href="/login"
+                    className={cn(
+                      buttonVariants({ variant: "secondary", size: "sm" }),
+                      "px-4"
+                    )}
+                  >
+                    Login
+                  </Link>
+                </nav>
+              </div>
+            </header> */}
+            <main className="flex-1">{children}</main>
+            {/* <SiteFooter /> */}
+          </div>
+        </ThemeProvider>  
+      </body>
     </html>
   )
 }
