2fd809e31b8c3ce2fc99acadf6eee4ad0cad4768
ananya150 <akblockchain15@gmail.com>
2023-06-23 14:17:23 +0530

    feat: added header helpers

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "Blogs",
+  description:
+    "A blogs app where i share my thoughts and learnings, build with Next.js 13.",
+  url: "",
+  ogImage: "",
+  links: {
+    twitter: "",
+    github: "",
+  },
+}
